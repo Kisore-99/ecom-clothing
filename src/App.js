4c5ad0135821ec0ce23c8e7444d56c2c48666b28
Kisore-99 <you@example.com>
2019-08-02 00:10:48 +0530

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
 import './App.css';
 
-const HatsPage= () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
+// const HatsPage= () =>(
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
 
-);
+// );
 // Route needs three things
 //component repreents which component to be navigated
 //path is the absolute path of that file
@@ -19,7 +21,7 @@ function App() {
     <div > 
       <Switch>
         <Route exact path='/' component={HomePage} /> 
-        <Route  path='/hats' component={HatsPage} /> 
+        <Route  path='/shop' component={ShopPage} /> 
       </Switch>
     </div>
   );
